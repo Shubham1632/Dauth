88cb2436f9f99cc46c3d3e9745b8fba4fcb1e896
Shubham Gangurde <79448312+Shubham1632@users.noreply.github.com>
2023-05-09 22:57:22 +0530

    added readme

---
+++ constants.js
@@ -0,0 +1,227 @@
+const abi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "string",
+        name: "alter_email",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "addr",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "number",
+        type: "uint256",
+      },
+    ],
+    name: "userCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_username",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_email",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_password",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_addr",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_alt_email",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_number",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "createUser",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "getUserNameByAddress",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_user",
+        type: "string",
+      },
+    ],
+    name: "getinfo",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "username",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "email",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "password",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "num",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct sso.user",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getusercount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "isUserOfAddress",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_user",
+        type: "string",
+      },
+    ],
+    name: "isUserOfName",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "userCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_user",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_password",
+        type: "string",
+      },
+    ],
+    name: "validate",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const contractAddress = "0x9037c252a112a097CfF6b81c1E9b31beF13dA9ef";
+
+const providerUrl =
+  "https://eth-sepolia.g.alchemy.com/v2/zJRZ96qtW_jI2M7f6Yo4RDz03DAmEplO";
+
+exports.abi = abi;
+exports.contractAddress = contractAddress;
+exports.providerUrl = providerUrl;
---
+++ index.js
@@ -0,0 +1,35 @@
+const { ethers } = require("ethers");
+const { contractAddress, abi, providerUrl } = require("./constants.js");
+
+class AuthenticationPackage {
+  constructor() {
+    this.provider = new ethers.providers.JsonRpcProvider(providerUrl);
+    this.authContract = new ethers.Contract(
+      contractAddress,
+      abi,
+      this.provider
+    );
+  }
+
+  async verifyUser(username, password) {
+    const result = await this.authContract.validate(username, password);
+    return result;
+  }
+
+  async IsUserOfAddress(address) {
+    const result = await this.authContract.isUserOfAddress(address);
+    return result;
+  }
+
+  async isUserOfName(userName) {
+    const result = await this.authContract.isUserOfName(userName);
+    return result;
+  }
+
+  async getUserNameByAddress(userAddress) {
+    const result = await this.authContract.getUserNameByAddress(userAddress);
+    return result;
+  }
+}
+
+module.exports = AuthenticationPackage;
